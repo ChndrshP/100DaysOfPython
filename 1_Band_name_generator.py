dbd49b2d0b3c56b31272fadbde514f14d32df60d
Chandresh Patel <99596026+ChndrshP@users.noreply.github.com>
2023-06-02 00:00:22 +0530

    Add files via upload

---
+++ 1_Band_name_generator.py
@@ -0,0 +1,20 @@
+#
+#
+#
+#                                THIS IS A BAND NAME GENERATOR 
+#
+#          
+#
+print ("Hey there, This is a Band name Generator")  #Print statement for initial greeting to user
+
+city = input("Enter your City you grew in : ")      #This statement takes the city name as input from the user
+
+pet = input("Enter your Pet's name : ")             #This statement takes the pet's name as input from the user
+
+print("Your Band name is " + city+pet +".")         #This statement prints the Band name
+#
+#
+#
+#               Combines the Users City and pets names to generate the Band name
+#
+#
